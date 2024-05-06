d3c51c0bd5b8b80b620b2fff1bbf215904b8ce62
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2024-05-06 18:34:09 -0300

    feat: open link in new tab

--- app/bibliography/page.tsx
+++ app/bibliography/page.tsx
@@ -9,23 +9,38 @@ export default function Bibliography() {
         </h1>
 
         <div className="w-full h-full flex flex-col gap-7 text-center text-white text-md text-wrap">
-          <Link href="https://www.iberdrola.com/inovacao/o-que-e-realidade-aumentada#:~:text=A%20Realidade%20Aumentada%20(RA)%20%C3%A9,bilh%C3%B5es%20de%20d%C3%B3lares%20no%20mundo">
+          <Link
+            href="https://www.iberdrola.com/inovacao/o-que-e-realidade-aumentada#:~:text=A%20Realidade%20Aumentada%20(RA)%20%C3%A9,bilh%C3%B5es%20de%20d%C3%B3lares%20no%20mundo"
+            target="_blank"
+          >
             https://www.iberdrola.com/inovacao/o-que-e-realidade-aumentada
           </Link>
 
-          <Link href="https://r2u.io/confira-algumas-empresas-de-realidade-virtual-e-realidade-aumentada-no-brasil/">
+          <Link
+            href="https://r2u.io/confira-algumas-empresas-de-realidade-virtual-e-realidade-aumentada-no-brasil/"
+            target="_blank"
+          >
             https://https://r2u.io/confira-algumas-empresas-de-realidade-virtual-e-realidade-aumentada-no-brasil/
           </Link>
 
-          <Link href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNQirHDzlyIyKC_tyaVY6LrQRBDm_tV_mc0GUq1VgguI3PKGPhFogsakJN&s=10">
+          <Link
+            href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNQirHDzlyIyKC_tyaVY6LrQRBDm_tV_mc0GUq1VgguI3PKGPhFogsakJN&s=10"
+            target="_blank"
+          >
             https://https://encrypted-tbn0.gstatic.com/
           </Link>
 
-          <Link href="https://sebrae.com.br/sites/PortalSebrae/artigos/realidadeaumentada-torna-mais-eficiente-a-industria-40,4d7bcf1a03fe5810VgnVCM1000001b00320aRCRD">
+          <Link
+            href="https://sebrae.com.br/sites/PortalSebrae/artigos/realidadeaumentada-torna-mais-eficiente-a-industria-40,4d7bcf1a03fe5810VgnVCM1000001b00320aRCRD"
+            target="_blank"
+          >
             https://sebrae.com.br/sites/PortalSebrae/artigos/realidadeaumentada-torna-mais-eficiente-a-industria-40
           </Link>
 
-          <Link href="https://www.redalyc.org/articulo.oa?id=50010104">
+          <Link
+            href="https://www.redalyc.org/articulo.oa?id=50010104"
+            target="_blank"
+          >
             Braga Mariluci . Realidade Virtual e Educação. Revista de Biologia e
             Ciências da Terra [em linea]. 2001, 1(1). ISSN: 1519-5228.
           </Link>
